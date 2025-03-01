81015b516b5bbb465f89749f418b2939bd8a891c
Maxplayz7566 <maxplayz7566yt@gmail.com>
2025-03-01 09:59:58 -0700

    Added working source code

--- api/main.py
+++ api/main.py
@@ -1,9 +1,11 @@
-from flask import Flask, jsonify
+from flask import Flask, request
+import requests
+
 app = Flask(__name__)
 
 @app.route('/api/hello')
 def hello(): 
-    return jsonify({'message': 'Hello from Python'})
+    return {'message': 'Hello from Python'}
 
 if __name__ == '__main__':
     app.run()
